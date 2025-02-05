973d15f906597793e992d14ea24e223207787d45
Suchanat Khumbungkhla <suchanat.k@clicknext.com>
2025-02-05 10:18:31 +0700

    feat(accordion): try accordion

---
+++ components/Accordion.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { cn } from "@/libs/utils";
+
+interface AccordionProps {
+  title: string;
+}
+
+defineProps<AccordionProps>();
+
+const openAccordion = ref(false);
+
+const toggleAccordion = () => {
+  openAccordion.value = !openAccordion.value;
+};
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-5">
+    <div
+      :class="
+        cn(
+          'flex items-center gap-x-4 p-2 rounded-md transition-all duration-400 cursor-pointer',
+          openAccordion ? 'bg-[#2E76C7]' : 'bg-[#F2F4F7]'
+        )
+      "
+      @click="toggleAccordion">
+      <div
+        :class="
+          cn(
+            'ml-1 w-[16px] h-[16px] bg-[#2E76C7] rounded-full transition-all duration-400',
+            openAccordion
+              ? 'transform rotate-180 bg-white'
+              : 'transform rotate-0 bg-[#2E76C7]'
+          )
+        "></div>
+
+      <span
+        :class="
+          cn(
+            'font-semibold transition-all duration-400 select-none',
+            openAccordion ? 'text-white' : 'text-black'
+          )
+        ">
+        {{ title }}
+      </span>
+    </div>
+
+    <template
+      v-if="openAccordion"
+      class="grid transition-all duration-400 ease-in-out">
+      <div class="flex flex-col">
+        <!-- <SubTHTable /> -->
+        <slot />
+      </div>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.th {
+  padding: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,106 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const tableData = {
+  sections: [
+    {
+      isHeader: true,
+      rows: [
+        {
+          cells: [
+            {
+              content: "ลำดับ",
+              rowspan: 2,
+              class: "rounded-tl-md",
+            },
+            {
+              content: "ฐานความผิด",
+              rowspan: 2,
+              class: "text-start",
+            },
+            {
+              content: "จำนวนผู้กระทำผิด (คน)",
+              rowspan: 2,
+            },
+            {
+              content: "จำนวนผู้ที่อาจจะกระทำความผิด(คน)",
+              colspan: 3,
+              class: "text-start border-1 border-blue-200 rounded-tr-md",
+            },
+          ],
+        },
+        {
+          cells: [
+            {
+              content: "1 ปี",
+              class: "p-2 border-x border-y border-blue-200",
+            },
+            {
+              content: "3 ปี",
+              class: "p-2 border-t border-blue-200",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      isHeader: false,
+      rows: [
+        {
+          cells: [
+            { content: "1" },
+            {
+              content:
+                "การกระทำที่ก่อให้เกิดอันตรายที่อาจเฉียบพลันหรือเรื้อรังต่อสุขภาพ",
+            },
+            { content: "2" },
+            { content: "6" },
+            { content: "6" },
+          ],
+        },
+        {
+          cells: [
+            { content: "2" },
+            {
+              content:
+                "การกระทำที่ก่อให้เกิดอันตรายที่อาจเฉียบพลันหรือเรื้อรังต่อสุขภาพ",
+            },
+            { content: "2" },
+            { content: "6" },
+            { content: "6" },
+          ],
+        },
+      ],
+    },
+  ],
+};
+</script>
 
 <template>
   <div class="w-full h-screen flex flex-col space-y-6 p-6">
+    <SubTHTable />
+
+    <SubTDTable />
+
+    <TestTable :sections="tableData.sections" />
+
     <Table />
 
-    <Table2 />
+    <div class="flex flex-col gap-y-4">
+      <h2 class="text-lg font-bold">ข้อมูลความผิด</h2>
+
+      <!-- <div class="flex flex-col gap-y-4">
+        <div v-for="(item, index) in 2"> -->
+      <Accordion
+        :title="`ฐานความผิดที่อาจจะกระทำผิดตามมาตรฐานระหว่างประเทศว่าด้วยการจำแนกประเภทอาชญากรรมเพื่อประโยชน์ทางสถิติ (ICCS)`">
+        <SubTHTable />
+      </Accordion>
+
+      <Accordion
+        :title="`ฐานความผิดที่อาจจะกระทำผิดตามมาตรฐานระหว่างประเทศว่าด้วยการจำแนกประเภทอาชญากรรมเพื่อประโยชน์ทางสถิติ (ICCS)`">
+        <SubTHTable />
+      </Accordion>
+      <!-- </div>
+      </div> -->
+    </div>
   </div>
 </template>
 
